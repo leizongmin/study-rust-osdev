16b19c48d08ab064be246a9623c0c3f82975e49e
Zongmin Lei <leizongmin@gmail.com>
2018-05-20 12:58:46 +0800

    first bvoot

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-#![feature(lang_items)]
-#![no_std]
-#![no_main]
+#![feature(lang_items)] // required for defining the panic handler
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
 
-#[lang = "panic_fmt"]
+#[lang = "panic_fmt"] // define a function that should be called on panic
 #[no_mangle]
 pub extern "C" fn rust_begin_panic(
     _msg: core::fmt::Arguments,
@@ -13,7 +13,20 @@ pub extern "C" fn rust_begin_panic(
     loop {}
 }
 
-#[no_mangle]
-pub extern "C" fn main() -> ! {
+static HELLO: &[u8] = b"================================================================================|                                                                              ||           FFF BB  III      FBI WARNING                                       ||           F   B B  I                                                         ||           FFF BB   I                                                         ||           F   B B  I                                                         ||           F   BB  III      https://potnhub.com                               ||                                                                              ||                                                                              |================================================================================";
+
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    // this function is the entry point, since the linker looks for a function
+    // named `_start` by default
+    let vga_buffer = 0xb8000 as *const u8 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
